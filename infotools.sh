fdac76d3ace0d076226a9bcda63d0cd74c680556
I must be strong <164299950+mestronger@users.noreply.github.com>
2024-03-27 00:53:40 +0700

    Update infotools.sh

--- infotools.sh
+++ infotools.sh
@@ -3,6 +3,6 @@
 echo "
 █ █▄░█ █▀▀ █▀█   ▀█▀ █▀█ █▀█ █░░ █▀
 █ █░▀█ █▀░ █▄█   ░█░ █▄█ █▄█ █▄▄ ▄█"
-    echo "1. fbreport: https://github-com/bhikandeshmukh/fbreport.git"
-    echo "2. fb crack: https://github-com/ZoraaCode/brutefb"
-    echo "3. ip track: https://github-com/htr-tech/track-ip.git"
+    echo "1. fbreport: http.//github-com/bhikandeshmukh/fbreport.git"
+    echo "2. fb crack: http.//github-com/ZoraaCode/brutefb"
+    echo "3. ip track: http.//github-com/htr-tech/track-ip.git"
