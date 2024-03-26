a5f9b8168530bce8e1ad2362b9045a5cc3ab4465
I must be strong <164299950+mestronger@users.noreply.github.com>
2024-03-27 00:27:55 +0700

    Update hackprank.php

--- hackprank.php
+++ hackprank.php
@@ -1,33 +1,29 @@
-<?php
-echo "
-██╗░░██╗░█████╗░░█████╗░██╗░░██╗
-██║░░██║██╔══██╗██╔══██╗██║░██╔╝
-███████║███████║██║░░╚═╝█████═╝░
-██╔══██║██╔══██║██║░░██╗██╔═██╗░
-██║░░██║██║░░██║╚█████╔╝██║░╚██╗
-╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝
+#!/bin/bash
 
-██████╗░██████╗░░█████╗░███╗░░██╗██╗░░██╗
-██╔══██╗██╔══██╗██╔══██╗████╗░██║██║░██╔╝
-██████╔╝██████╔╝███████║██╔██╗██║█████═╝░
-██╔═══╝░██╔══██╗██╔══██║██║╚████║██╔═██╗░
-██║░░░░░██║░░██║██║░░██║██║░╚███║██║░╚██╗
-╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝
+# Fungsi untuk menampilkan menu
+show_menu() {
+    clear
+    echo "REKOMENDASI TOOLS WORK"
+    echo "1. Facebook Report"
+    echo "2. Facebook Crack"
+    echo "3. Ip Tracker"
+    echo "4. Keluar"
+}
 
-██████╗░██████╗░░█████╗░
-██╔══██╗██╔══██╗██╔══██╗
-██████╔╝██████╔╝██║░░██║
-██╔═══╝░██╔══██╗██║░░██║
-██║░░░░░██║░░██║╚█████╔╝
-╚═╝░░░░░╚═╝░░╚═╝░╚════╝░";
-<br>
-echo "WELCOME HACKING PRANK PRO";
-<br>
-echo "1. input url:";
-<br>
-echo "2. input username:";
-<br>
-echo "3. input email:";
-<br>
-echo "4. input phone number:";
-?>
+# Fungsi untuk meminta masukan pengguna dan menanggapi masukan
+get_input() {
+    read -p "Masukkan pilihan Anda: " choice
+    case $choice in
+        1) echo "https://github.com/bhikandeshmukh/fbreport.git";;
+        2) echo "https://github.com/ZoraaCode/brutefb";;
+        3) echo "https://github.com/htr-tech/track-ip.git";;
+        4) exit;;
+        *) echo "Pilihan tidak valid";;
+    esac
+}
+
+# Main program
+while true; do
+    show_menu
+    get_input
+done
