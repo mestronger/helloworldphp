1b7dda4f716fee9f29f5a0b91519d123f227d991
I must be strong <164299950+mestronger@users.noreply.github.com>
2024-03-27 00:47:25 +0700

    Update infotools.sh

--- infotools.sh
+++ infotools.sh
@@ -5,30 +5,8 @@ echo "
 █ █░▀█ █▀░ █▄█   ░█░ █▄█ █▄█ █▄▄ ▄█"
 
 
-# Fungsi untuk menampilkan
 show_menu() {
-    clear
-    echo "REKOMENDASI TOOLS WORK"
-    echo "1. Facebook Report"
-    echo "2. Facebook Crack"
-    echo "3. Ip Tracker"
-    echo "4. Keluar"
+    echo "1. fbreport: https://github-com/bhikandeshmukh/fbreport.git"
+    echo "2. fb crack: https://github-com/ZoraaCode/brutefb"
+    echo "3. ip track: https://github-com/htr-tech/track-ip.git"
 }
-
-# Fungsi untuk meminta masukan pengguna dan menanggapi masukan
-get_input() {
-    read -p "Masukkan pilihan Anda: " choice
-    case $choice in
-        1) echo "https.//github-com/bhikandeshmukh/fbreport.git";;
-        2) echo "https.//github-com/ZoraaCode/brutefb";;
-        3) echo "https.//github-com/htr-tech/track-ip.git";;
-        4) exit;;
-        *) echo "Pilihan tidak valid";;
-    esac
-}
-
-# Main program
-while true; do
-    show_menu
-    get_input
-done
